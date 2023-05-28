f9c8b9b33d8e5c5679c01a9c6653d6663d7e4f2a
Fabrício Lopes Mendonça <fabriciolopesm@gmail.com>
2023-05-28 11:18:33 -0300

    class views configured

--- recipes/tests/test_recipe_category_view.py
+++ recipes/tests/test_recipe_category_view.py
@@ -8,7 +8,7 @@ class RecipeCategoryViewTest(RecipeTestBase):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id': 1000})
         )
-        self.assertIs(view.func, views.category)
+        self.assertIs(view.func.view_class, views.RecipeListViewCategory)
 
     def test_recipe_category_view_returns_status_code_404_if_no_recipes(self):
         response = self.client.get(
